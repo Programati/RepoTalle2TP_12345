7c687c2c9a1e5d9bf301d4ff59d65ab48d6074d8
Matias <mati.j.martinez@gmail.com>
2024-08-11 15:59:13 -0300

    detalles

--- TP2/Practico2/obj/Debug/net8.0-windows/Practico2.AssemblyInfo.cs
+++ TP2/Practico2/obj/Debug/net8.0-windows/Practico2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practico2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+377666734ebacbdcba604fde6aac1959dd762cd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practico2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practico2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
