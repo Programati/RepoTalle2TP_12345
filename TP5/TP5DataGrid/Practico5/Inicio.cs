ef607f6158cea48f81ef4ef027af9ef5fcc8fd41
Matias <mati.j.martinez@gmail.com>
2024-08-27 11:33:40 -0300

    FinalizacionTP5

--- TP5/TP5DataGrid/Practico5/Inicio.cs
+++ TP5/TP5DataGrid/Practico5/Inicio.cs
@@ -35,63 +35,73 @@ namespace Practico5
             }
             else
             {
-                //Adicionamos un nuevo renglon
-                int n = dtgvCliente.Rows.Add();
-                TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+                // Obtener la fecha seleccionada del DateTimePicker
+                DateTime fechaSeleccionada = dateTimePicker1.Value.Date;
+
+                // Calcular la fecha mínima permitida (18 años atrás desde hoy)
+                DateTime fechaMinima = DateTime.Today.AddYears(-18);
+                if (fechaSeleccionada <= fechaMinima)
+                {
+
+                    //Adicionamos un nuevo renglon
+                    int n = dtgvCliente.Rows.Add();
+                    TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
                 
-                //Colocamos la información en el renglon creado
-                dtgvCliente.Rows[n].Cells[0].Value = textInfo.ToTitleCase(txtApellido.Text.ToLower());
-                dtgvCliente.Rows[n].Cells[1].Value = textInfo.ToTitleCase(txtNombre.Text.ToLower());
-                dtgvCliente.Rows[n].Cells[2].Value = dateTimePicker1.Text;
+                    //Colocamos la información en el renglon creado
+                    dtgvCliente.Rows[n].Cells[0].Value = textInfo.ToTitleCase(txtApellido.Text.ToLower());
+                    dtgvCliente.Rows[n].Cells[1].Value = textInfo.ToTitleCase(txtNombre.Text.ToLower());
+                    dtgvCliente.Rows[n].Cells[2].Value = dateTimePicker1.Text;
 
-                if(rdbHombre.Checked)dtgvCliente.Rows[n].Cells[3].Value = "Hombre";
-                else dtgvCliente.Rows[n].Cells[3].Value = "Mujer";
+                    if(rdbHombre.Checked)dtgvCliente.Rows[n].Cells[3].Value = "Hombre";
+                    else dtgvCliente.Rows[n].Cells[3].Value = "Mujer";
 
-                dtgvCliente.Rows[n].Cells[4].Value = "Eliminar";
+                    dtgvCliente.Rows[n].Cells[4].Value = "Eliminar";
 
-                if (Decimal.Parse(txtSaldo.Text) < 50)dtgvCliente.Rows[n].DefaultCellStyle.BackColor = Color.Red;
-                dtgvCliente.Rows[n].Cells[5].Value = Decimal.Parse(txtSaldo.Text);
+                    if (Decimal.Parse(txtSaldo.Text) < 50)dtgvCliente.Rows[n].DefaultCellStyle.BackColor = Color.Red;
+                    dtgvCliente.Rows[n].Cells[5].Value = Decimal.Parse(txtSaldo.Text);
                                     
-                try
-                {
-                    // Definir la ruta de la carpeta "Fotos"
-                    string fotosPath = Path.Combine(ejecutablePath, "Fotos");
-
-                    // Verificar si la carpeta "Fotos" existe, si no, crearla
-                    if (!Directory.Exists(fotosPath))
+                    try
                     {
-                        Directory.CreateDirectory(fotosPath);
+                        // Definir la ruta de la carpeta "Fotos"
+                        string fotosPath = Path.Combine(ejecutablePath, "Fotos");
+
+                        // Verificar si la carpeta "Fotos" existe, si no, crearla
+                        if (!Directory.Exists(fotosPath))
+                        {
+                            Directory.CreateDirectory(fotosPath);
+                        }
+
+                        // Obtener la ruta del archivo seleccionado desde el TextBox `txtfoto`
+                        string sourceFilePath = txtFoto.Text;
+
+                        // Obtener el nombre del archivo seleccionado
+                        string fileName = Path.GetFileName(sourceFilePath);
+
+                        // Definir la ruta completa de destino donde se copiará la imagen
+                        destinationFilePath = Path.Combine(fotosPath, txtApellido.Text+txtNombre.Text+n+fileName);
+
+                        // Copiar el archivo a la carpeta "Fotos"
+                        File.Copy(sourceFilePath, destinationFilePath, true);
+
                     }
+                    catch (Exception ex)
+                    {
+                        // Manejar cualquier error que ocurra durante el proceso
+                        MessageBox.Show("Ocurrió un error al guardar la imagen: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    Image imagen = Image.FromFile(destinationFilePath);
+                    dtgvCliente.Rows[n].Cells[6].Value = imagen;
 
-                    // Obtener la ruta del archivo seleccionado desde el TextBox `txtfoto`
-                    string sourceFilePath = txtFoto.Text;
+                    //Ponemos la ruta en la ultima celda
+                    dtgvCliente.Rows[n].Cells[7].Value = destinationFilePath;
 
-                    // Obtener el nombre del archivo seleccionado
-                    string fileName = Path.GetFileName(sourceFilePath);
-
-                    // Definir la ruta completa de destino donde se copiará la imagen
-                    destinationFilePath = Path.Combine(fotosPath, txtApellido.Text+txtNombre.Text+n+fileName);
-
-                    // Copiar el archivo a la carpeta "Fotos"
-                    File.Copy(sourceFilePath, destinationFilePath, true);
-
-                    // Informar al usuario que la imagen se guardó con éxito
-                    MessageBox.Show("La imagen se ha guardado correctamente en la carpeta 'Fotos'.", "Guardado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    limpiarCampos();
+                    destinationFilePath = null;
                 }
-                catch (Exception ex)
+                else
                 {
-                    // Manejar cualquier error que ocurra durante el proceso
-                    MessageBox.Show("Ocurrió un error al guardar la imagen: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    MessageBox.Show("Usted tiene menos de 18 años, no puedes ser un cliente!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
                 }
-                Image imagen = Image.FromFile(destinationFilePath);
-                dtgvCliente.Rows[n].Cells[6].Value = imagen;
-
-                //Ponemos la ruta en la ultima celda
-                dtgvCliente.Rows[n].Cells[7].Value = destinationFilePath;
-                //string imagePath = pictureBox1.ImageLocation;
-
-                limpiarCampos();
-                destinationFilePath = null;
             }
 
         }
@@ -163,41 +173,9 @@ namespace Practico5
                     DialogResult ask = MessageBox.Show("Está a punto de eliminar al Cliente: " + cliente, "Confirmar Eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2);
                     if (ask == DialogResult.Yes)
                     {
-                        dtgvCliente.Rows[n].Cells[6].Value = null;
-                        dtgvCliente.Rows[n].Cells[6].Dispose();
-
-
-                        // Definir la ruta de la carpeta "Fotos"
-                        string fotosPath = Path.Combine(ejecutablePath, "Fotos");
-
-                        // Obtener el nombre del archivo seleccionado
-                        string fileName = Path.GetFileName(dtgvCliente.Rows[n].Cells[7].Value.ToString());
-                        
-                        // Definir la ruta completa del archivo en la carpeta "Fotos"
-                        string filePath = Path.Combine(fotosPath, fileName);
-                        
-                        // Verificar si el archivo existe
-                        // Liberar el archivo si está cargado en un PictureBox
-                        if (pictureBox1.Image != null)
-                            {
-                                pictureBox1.Image.Dispose();
-                                pictureBox1.Image = null;
-                            }
-                            
-                        MessageBox.Show("El Cliente: " + cliente + " se eliminó correctamente", "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Information);
                         dtgvCliente.Rows.RemoveAt(n);
-                        limpiarCampos();
-                        if (File.Exists(filePath))
-                        {                            
-                            // Eliminar el archivo
-                            //File.Delete(filePath);
-                        }
-                        else
-                        {
-                            // Informar al usuario que el archivo no existe
-                            MessageBox.Show("La imagen no se encontró en la carpeta 'Fotos'.", "Archivo no encontrado", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        }
                      
+                        limpiarCampos(); 
                     }
 
                 }
